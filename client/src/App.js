a12d8337a0233a0dc1214716d9085f4e251fe91e
Alan <alanbuckle05@gmail.com>
2018-10-09 11:23:52 -0400

    added category list

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './components/Home'
 
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Home />
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Home}/>
+        </Switch>
+      </Router>
     )
   }
 }
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
 
 export default class Home extends Component {
   render() {
@@ -6,6 +8,18 @@ export default class Home extends Component {
       <div>
         <h1>Vent-It</h1>
         <h2>Categories</h2>
+        <ul>
+          <li><Link to=''>Traffic</Link></li>
+          <li><Link to=''>Work</Link></li>
+          <li><Link to=''>Friends</Link></li>
+          <li><Link to=''>Relationships</Link></li>
+          <li><Link to=''>Misc</Link></li>
+          <li><Link to=''>Life</Link></li>
+          <li><Link to=''>Make a new category</Link></li>
+
+
+
+        </ul>
       </div>
     )
   }
