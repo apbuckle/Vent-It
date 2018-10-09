e393ea28bf1bfc31c54fa9ec065918349fbc46e0
Alan <alanbuckle05@gmail.com>
2018-10-09 16:46:23 -0400

    Category link works to direct to Vent page

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './components/Home'
 import Category from './components/Category'
-// import Vent from './components/Vent'
+import Vent from './components/Vent'
 
 
 class App extends Component {
@@ -12,7 +12,7 @@ class App extends Component {
         <Switch>
           <Route exact path='/' component={Home}/>
           <Route exact path='/category' component={Category} />
-          {/* <Route exact path='/categories/:categoryId' component={Vent} /> */}
+          <Route exact path='/categories/:categoryId' component={Vent} />
         </Switch>
       </Router>
     )
