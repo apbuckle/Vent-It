1bb5a65635875066550cb2e67121a1f44b124564
Alan <alanbuckle05@gmail.com>
2018-10-10 22:39:51 -0400

    Css on vent page, text field with scroll bar and expanding properties.

--- client/src/components/Vent.js
+++ client/src/components/Vent.js
@@ -1,7 +1,73 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-// import styled from 'styled-components'
-import { StyledLink } from './SharedComponents'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  height: 100%;
+  width: 100%;
+  background-color: #00BBAA;
+`
+const StyledH = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 25px;
+  font-size: 12vw;
+  text-shadow: .4vw .4vw 0 #d32f2f; 
+  font-weight: bold;
+`
+const StyledBoard = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  width: 70vw;
+  padding: 10px;
+  flex-wrap: wrap;
+`
+
+const StyledVent = styled.div`
+  background-color: #9a0007;
+  max-width: 150px;
+  padding: 15px;
+  padding-top: 0px;
+  margin: 15px;
+  pre {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    border: 0;
+  }
+  .expandingArea {
+    position: relative;
+    border: 1px solid black;
+  }
+  textarea {
+    box-sizing: border-box;
+    width: 100%;
+    height: 100px;
+  }
+
+`
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  font-size: 3vw;
+  color: Black;
+  `
+const StyledAdd = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 15px;
+  font-size: 10vw;
+  text-shadow: .2vw .2vw 0 #d32f2f;
+`
+
 
 
 
@@ -51,31 +117,37 @@ export default class Vent extends Component {
   render() {
     const ventsList = this.state.vents.map((vent, i) => {
       return (
-        <div key={i}>
-          <div onClick={() => this.handleDelete(vent._id)}> X </div>
+        <StyledVent key={i}>
+          {/* <StyledNav onClick={() => this.handleDelete(vent._id)}> - </StyledNav> */}
 
-          <input type='text' name='title' placeholder='title' value={vent.title}
+          {/* <input type='text' name='title' placeholder='title' value={vent.title}
             onChange={(event) => this.handleChange(event, i)}
-            onBlur={() => this.updateVent(i)} />
+            onBlur={() => this.updateVent(i)} /> */}
+          
+            <div class='expandingArea'>
+              <pre><span></span></pre>
+              <textarea></textarea>
+            </div>
 
-          <input type='text' name='description' placeholder='description' value={vent.description}
+          {/* <input type='text' name='description' placeholder='Message' value={vent.description}
             onChange={(event) => this.handleChange(event, i)}
-            onBlur={() => this.updateVent(i)} />
-        </div>
+            onBlur={() => this.updateVent(i)} /> */}
+         
+        </StyledVent>
       )
     })
 
     return (
-      <div>
-        <h1> : {this.state.category.title}</h1>
+      <StyledContainer>
+        <StyledH> : {this.state.category.title}</StyledH>
         <div>
-          <div onClick={this.handleNew}>New Vent</div>
+          <StyledAdd onClick={this.handleNew}>+</StyledAdd>
         </div>
-        <div>
+        <StyledBoard>
           {ventsList}
-        </div>
-        <StyledLink to='/category'>Return to category</StyledLink>
-      </div>
+        </StyledBoard>
+        <StyledLink to='/category'> Back </StyledLink>
+      </StyledContainer>
     )
   }
 }
